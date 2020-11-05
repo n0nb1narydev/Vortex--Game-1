50209ac3f02f4d1594b662c63b36cc24ef391346
Mel Rumsey <melsrumsey@gmail.com>
2020-11-05 08:32:25 -0600

    save

--- Vortex/Assets/Scripts/Tornado.cs
+++ Vortex/Assets/Scripts/Tornado.cs
@@ -1,203 +1,30 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class Tornado : MonoBehaviour
 {
-	//set position and rotation
-	private Vector3 initialPosition;
-	private Quaternion initialRotation;
-	private bool isWind = false;
-	private bool isNormalThrow = false;
-	private bool isRotateLeft = true;
+	[SerializeField]
+	private float _speed = 2f;
+	[SerializeField]
+	private Button _launch;
+	private bool _isMoving = false;
 
-	//setup floats
-	private float curveWind = 1f;
-	private float curveForce = 0f;
-	public float objectScale = 1.0f;
-	private float throwPos = 0f;
-
-	private Transform childBall;
-	private bool isCurve = false;
-	private Rigidbody rigid;
-	private bool ballThrowed= false;
-	public Vector3 initialScale;
-	private IEnumerator jumpCoroutine;
-	private bool isRotate = false;
-	public Camera cam;
-
-
-
-	void OnEnable ()
+	private void Start() 
 	{
-		//set default position when object enable
-		initialPosition = transform.position;
+
 	}
 
-	void Start ()
+	void Update()
 	{
-		rigid = GetComponent<Rigidbody> ();
-		//initalize gradually jump coroutine
-		jumpCoroutine = JumpAround (0.3f);
-		childBall = transform.Find ("Ball");
-		StartCoroutine (jumpCoroutine);
-
-		//set initial scale, 
-		initialScale = transform.localScale; 
-		
-		// if no cam attached, use the default camera
-		if (cam == null)
-			cam = Camera.main; 
-	}
-	
-	void FixedUpdate ()
-	{
-		//apply wind after certain distance
-		float dist = 0f;
-		if (ThrowTornadoController.Instance.target) {
-			dist = (ThrowTornadoController.Instance.target.position.z - transform.position.z);
-		}
-
-		//if throw is cureve then apply wind
-		if (isCurve && dist <= (throwPos - (throwPos / 9.5f))) {
-			if (!isWind)
-				StartCoroutine (wind (0.5f));
-			transform.Translate (Vector3.right * -curveForce * curveWind * Time.deltaTime);
-		}
+		transform.Translate ( Vector3.up * _speed * Time.deltaTime );
 	}
 
 
 
-	private void isThrow (bool flag)
-	{
-		if (jumpCoroutine != null) {
-			StopCoroutine (jumpCoroutine);
-			jumpCoroutine = null;
-		}
-		ballThrowed= flag;
-		if (ThrowTornadoController.Instance.IsGettingDirection)
-			isRotate = false;
-		else
-			isRotate = true;
-		throwPos = (ThrowTornadoController.Instance.target.position.z - transform.position.z);
-	}
-
-
-	//Resets the ball.
-	public void ResetBall ()
-	{
-		ballThrowed= false;
-		StopAllCoroutines ();
-		//ball move to initial position
-		StartCoroutine (GobackToDefaultPosition (0.3f));
-	}
-		
-	//Sets the curve.
-	private void SetCurve (float cFactore)
-	{
-		curveForce = cFactore;
-		isCurve = true;
-		Debug.Log("This is a Curve Throw");
-	}
-		
-
-	//the wind logic
-	IEnumerator wind (float t)
-	{
-		isWind = true;
-		float rate = 1.0f / t;
-		float i = 0f;
-		while (i<1.0f) {
-			i += rate * Time.deltaTime;
-			curveWind = Mathf.Lerp (1, 26, i);
-			yield return 0;
-		}
-	}
-
-
-	/// Winds the reverse.
-	IEnumerator windReverse (float t)
-	{
-		isWind = true;
-		float rate = 1.0f / t;
-		float i = 0f;
-		while (i<1.0f) {
-			i += rate * Time.deltaTime;
-			curveWind = Mathf.Lerp (26, 0, i);
-			yield return 0;
-		}
-	}
-
-	//ball jumping animation
-	IEnumerator JumpAround (float tm)
+	void MoveUp()
 	{
 
-		while (!ballThrowed) {
-
-			yield return new WaitForSeconds (0.4f);
-		
-			transform.position = initialPosition;
-
-			if (ThrowTornadoController.Instance.IsGameStart) {
-
-
-				isRotateLeft = !isRotateLeft;
-				isRotate = true;
-				float i = 0f;
-				float rate = 1.0f / tm;
-				Vector3 from = initialPosition;
-				Vector3 to = new Vector3 (from.x, from.y + 0.05f, from.z);
-	
-				while (i<1.0f) {
-					i += rate * Time.deltaTime;
-					transform.position = Vector3.Lerp (from, to, i);
-					yield return 0f;
-				}
-				i = 0f;
-				rate = 1.0f / (tm / 0.7f);
-
-				Vector3 bump = from;
-				bump.y -= 0.05f;
-
-				while (i<1.0f) {
-					i += rate * Time.deltaTime;
-					transform.position = Vector3.Lerp (to, bump, i);
-					yield return 0f;
-				}
-
-				isRotate = false;
-
-				i = 0f;
-				rate = 1.0f / (tm / 1.1f);
-
-				while (i<1.0f) {
-					i += rate * Time.deltaTime;
-					transform.position = Vector3.Lerp (bump, from, i);
-					yield return 0f;
-				}
-
-			}
-		}
+		_isMoving = true;
 	}
-
-
-	//The back to initial position
-	IEnumerator GobackToDefaultPosition (float tm)
-	{
-		float i = 0f;
-		float rate = 1.0f / tm;
-		Vector3 from = transform.position;
-		Vector3 to = initialPosition;
-		while (i<1.0f) {
-			i += rate * Time.deltaTime;
-			transform.position = Vector3.Lerp (from, to, i);
-			yield return 0f;
-		}
-		transform.position = initialPosition;
-		childBall.localRotation = Quaternion.identity;
-		isRotate = false;
-		jumpCoroutine = JumpAround (0.3f);
-		
-		StartCoroutine (jumpCoroutine);
-	}
-
 }
